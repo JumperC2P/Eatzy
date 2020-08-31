40bee62f2bd6ac42c44cc6de265d9641e34ef48d
Mary-Biu-Biu-Biu <63097054+Mary-Biu-Biu-Biu@users.noreply.github.com>
2020-08-31 00:55:50 -0700

    completed grocery list part with proper comments

--- Eatzy/ViewModels/GroceryList/GroceryListViewController.swift
+++ Eatzy/ViewModels/GroceryList/GroceryListViewController.swift
@@ -15,13 +15,9 @@ class GroceryListViewController: UIViewController {
     var editIndex: Int?
     @IBOutlet weak var AddItemTextField: UITextField!
     @IBOutlet weak var GroceryListView: UITableView!
-    
-
     @IBOutlet weak var newName: UITextField!
     @IBOutlet weak var newAmount: UITextField!
-    
-//    @IBOutlet weak var GroceryCell: UITableViewCell!
-    
+ 
     // add new items from user
     @IBAction func AddItemButtonPressed(_ sender: UIButton) {
         let amount = newAmount.text
@@ -40,34 +36,28 @@ class GroceryListViewController: UIViewController {
         }
         
     }
-    
 
-//    var items:[Grocery] = [
-//        Grocery(name: "Ground Beef", amount: "1 lb"),
-//        Grocery(name: "Crushed Tomatoes", amount: "1 can")
-//    ]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         GroceryListView.dataSource = self
         GroceryListView.delegate = self
         
-        // style table view
+        // style table view: cancel the separators for empty cells
         GroceryListView.tableFooterView = UIView.init(frame: .zero)
         let backgroundView = UIView()
         backgroundView.backgroundColor = #colorLiteral(red: 0.4235294118, green: 0.4823529412, blue: 0.5843137255, alpha: 1)
 
-        // load data
+        // load initial data (mock-up)
         GroceryViewModel.loadGroceryItems()
         
-        // allow multiple selection in table
+        // only allow single selection in table view
         self.GroceryListView.allowsSelection = true;
         self.GroceryListView.allowsMultipleSelection = false;
-//        self.GroceryListView.allowsMultipleSelectionDuringEditing = true
+        
     }
     
-    // segue for editing items, pass essential information
+    // segue for editing items, pass essential information and functions
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "EditItemSegue"{
             let popup = segue.destination as! EditItemViewController
@@ -77,16 +67,7 @@ class GroceryListViewController: UIViewController {
             }
         }
     }
-    
 
-    
-    // ??????????
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete{
-//            self.items.remove(at: indexPath.row)
-//
-//        }
-//    }
 
     
 }
@@ -102,21 +83,12 @@ extension GroceryListViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GroceryCell", for: indexPath)
         let current = GroceryViewModel.getGroceryItem(byIndex: indexPath.row)
-        cell.textLabel?.text = current.name
         cell.detailTextLabel?.text = current.amount
-//        cell.selectedBackgroundView = UIView()
-//        cell.selectedBackgroundView?.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-//        cell.textLabel?.highlightedTextColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-//        if(cell.isSelected){
-//            let attrStr = NSMutableAttributedString.init(string: current.name)
-//            attrStr.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSNumber.init(value: 1), range: NSRange.init(location:0, length: current.name.count))
-//            cell.textLabel?.attributedText = attrStr
-//        }
-        
+
+        // if current record is completed, then render it with strikethrough and grey background
+        // otherwise, render with normal text
         if(current.complete){
             print("item completed")
-//            cell.selectedBackgroundView = UIView()
-//            cell.selectedBackgroundView?.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
             let attrStr = NSMutableAttributedString.init(string: current.name)
             attrStr.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSNumber.init(value: 1), range: NSRange.init(location:0, length: current.name.count))
             cell.textLabel?.attributedText = attrStr
@@ -133,40 +105,15 @@ extension GroceryListViewController:UITableViewDataSource{
 
 
 extension GroceryListViewController: UITableViewDelegate{
-
-    // get the item selected
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath:IndexPath){
-//        self.selectDeselectCell(tableView: GroceryListView, indexPath: indexPath)
-//        GroceryViewModel.changeItemStatus(byIndex: indexPath.row)
-//        print("select + \(GroceryListView.cellForRow(at: indexPath)?.isSelected)")
-//        self.GroceryListView.reloadData()
-    }
     
-    // get the item deselected
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-//        self.selectDeselectCell(tableView: GroceryListView, indexPath: indexPath)
-//        var currentItem = GroceryViewModel.getGroceryItem(byIndex: indexPath.row)
-//        currentItem.complete = false
-//        print("deselect + \(GroceryListView.cellForRow(at: indexPath)?.isSelected)")
-//        GroceryViewModel.updateCompleteStatus(byIndex: indexPath.row, complete: false)
-//        self.GroceryListView.reloadData()
-    }
-    
-    
-    func selectDeselectCell(tableView:UITableView, indexPath:IndexPath){
-        self.selectArr.removeAll()
-        if let arr = GroceryListView.indexPathForSelectedRow{
-            print(arr)
-        }
-    }
-    
-    // swipe from right hand side
+    // swipe from right hand side: edit and delete
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let edit = editAction(at: indexPath)
         let delete = deleteAction(at: indexPath)
         return UISwipeActionsConfiguration(actions: [delete,edit])
     }
     
+    // swipe from left hand side: compelete or undo
     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let complete = completeAction(at: indexPath)
         return UISwipeActionsConfiguration(actions: [complete])
@@ -198,29 +145,27 @@ extension GroceryListViewController: UITableViewDelegate{
         return action
     }
     
+    // complete/undo button
     func completeAction(at indexPath: IndexPath) -> UIContextualAction {
-        
+        // if current cell is completed, render undo button
         if(GroceryViewModel.getStatus(byIndex: indexPath.row)){
             let action = UIContextualAction(style: .normal, title:"Undo"){
                 (action,view,completion) in
                 GroceryViewModel.changeItemStatus(byIndex: indexPath.row)
-                //            self.GroceryListView.reloadData()
                 self.GroceryListView.reloadRows(at: [indexPath], with: .automatic)
                 completion(true)
             }
-            
             action.backgroundColor = #colorLiteral(red: 0.9607843137, green: 0.6392156863, blue: 0.1019607843, alpha: 1)
             return action
         }
+        // otherwise, render complete button
         else{
             let action = UIContextualAction(style: .normal, title:"Complete"){
                 (action,view,completion) in
                 GroceryViewModel.changeItemStatus(byIndex: indexPath.row)
-                //            self.GroceryListView.reloadData()
                 self.GroceryListView.reloadRows(at: [indexPath], with: .automatic)
                 completion(true)
             }
-            
             action.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
             return action
         }
